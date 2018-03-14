c371167d1f0996f02b01ce2e59b38acd63c0c282
Emil Englesson <englesson.emil@gmail.com>
2018-03-14 18:23:36 +0100

    Added two very basic tests
    Signed-off-by: Emil Englesson <englesson.emil@gmail.com>

--- src/test/java/heap/HeapTest.java
+++ src/test/java/heap/HeapTest.java
@@ -1,21 +1,36 @@
 package heap;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public abstract class HeapTest<T extends Heap<Integer>> {
 
-	protected T stack;
+	protected T heap;
 
 	protected abstract T createInstance(int order);
 
 	@Before
 	public void setUp() {
-		stack = createInstance(3);
+		heap = createInstance(3);
 	}
 
 	@After
 	public void tearDown() {
-		stack = null;
+		heap = null;
+	}
+
+	@Test
+	public void testEmptyWhenCreated() {
+		assertTrue(heap.isEmpty());
+	}
+
+	@Test
+	public void testNotEmptyAfterAdd() {
+		heap.offer(50);
+		assertFalse(heap.isEmpty());
 	}
 }

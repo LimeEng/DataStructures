50fffe7aba5a95951c60ad9da4fb61339513f1de
LimeEng <35342122+LimeEng@users.noreply.github.com>
2018-03-16 21:45:30 +0100

    The constructors are now all implemented in the same way, and covered by tests (#24)
    Signed-off-by: Emil Englesson <englesson.emil@gmail.com>

--- src/test/java/stack/LinkedStackTest.java
+++ src/test/java/stack/LinkedStackTest.java
@@ -1,9 +1,16 @@
 package stack;
 
+import java.util.Collection;
+
 public class LinkedStackTest extends StackTest<LinkedStack<Integer>> {
 	
 	@Override
 	protected LinkedStack<Integer> createInstance() {
 		return new LinkedStack<>();
+	}
+
+	@Override
+	protected LinkedStack<Integer> createInstance(Collection<Integer> c) {
+		return new LinkedStack<>(c);
 	}
 }

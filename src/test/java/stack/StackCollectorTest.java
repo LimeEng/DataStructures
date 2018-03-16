9332ffdc902c24ae5773fc0205681ab9f3aeb03d
LimeEng <35342122+LimeEng@users.noreply.github.com>
2018-03-16 21:54:13 +0100

    Added a test case and fixed another test (#25)
    Signed-off-by: Emil Englesson <englesson.emil@gmail.com>

--- src/test/java/stack/StackCollectorTest.java
+++ src/test/java/stack/StackCollectorTest.java
@@ -13,40 +13,59 @@ public class StackCollectorTest {
 
 	@Test
 	public void testCollectToStack() {
-		Stack<Integer> stack = IntStream.range(0, 100)
+		Stack<Integer> stack = IntStream.range(0, 1000)
 				.boxed()
 				.collect(StackCollector.toStack());
-		assertEquals("The collected stack is the wrong size", 100, stack.size());
+		assertEquals("The collected stack is the wrong size", 1000, stack.size());
+		List<Integer> values = stack.popWhile(e -> true);
+		List<Integer> expected = IntStream.range(0, 1000)
+				.boxed()
+				.collect(Collectors.toList());
+		Collections.reverse(expected);
+		assertEquals("The collector returned a malformed stack", expected, values);
+	}
+
+	@Test
+	public void testCollectToStackParallell() {
+		Stack<Integer> stack = IntStream.range(0, 1000)
+				.parallel()
+				.boxed()
+				.collect(StackCollector.toStack());
+		assertEquals("The collected stack is the wrong size", 1000, stack.size());
 		List<Integer> values = stack.popWhile(e -> true);
 		Collections.sort(values);
-		List<Integer> expected = IntStream.range(0, 100)
+		List<Integer> expected = IntStream.range(0, 1000)
 				.boxed()
 				.collect(Collectors.toList());
 		assertEquals("The collector returned a malformed stack", expected, values);
 	}
-	
+
 	@Test
 	public void testToStackNotNull() {
 		assertNotNull("toStack() returns null", StackCollector.toStack());
 	}
-	
+
 	@Test
 	public void testCharacteristicsNotNull() {
-		assertNotNull("toStack() returns null", StackCollector.toStack().characteristics());
+		assertNotNull("toStack() returns null", StackCollector.toStack()
+				.characteristics());
 	}
-	
+
 	@Test
 	public void testFinisherNotNull() {
-		assertNotNull("toStack() returns null", StackCollector.toStack().finisher());
+		assertNotNull("toStack() returns null", StackCollector.toStack()
+				.finisher());
 	}
-	
+
 	@Test
 	public void testCombinerNotNull() {
-		assertNotNull("toStack() returns null", StackCollector.toStack().combiner());
+		assertNotNull("toStack() returns null", StackCollector.toStack()
+				.combiner());
 	}
-	
+
 	@Test
 	public void testAccumulatorNotNull() {
-		assertNotNull("toStack() returns null", StackCollector.toStack().accumulator());
+		assertNotNull("toStack() returns null", StackCollector.toStack()
+				.accumulator());
 	}
 }

ce33f4db8cff0dde5f321d2d0080840d8dba9b19
Emil Englesson <englesson.emil@gmail.com>
2018-03-21 15:18:03 +0100

    Revert "Added one basic test"
    This reverts commit 2513c560dea44df3e65138c332a8da2571812978.

--- src/test/java/heap/ArrayHeapTest.java
+++ src/test/java/heap/ArrayHeapTest.java
@@ -2,8 +2,6 @@ package heap;
 
 import static org.junit.Assert.*;
 
-import java.util.stream.IntStream;
-
 import org.junit.Test;
 
 public class ArrayHeapTest extends HeapTest<ArrayHeap<Integer>> {
@@ -26,21 +24,5 @@ public class ArrayHeapTest extends HeapTest<ArrayHeap<Integer>> {
 			exceptionThrown = true;
 		}
 		assertTrue("No exception thrown for trying to create a heap with order 0", exceptionThrown);
-	}
-
-	@Test
-	public void testComparatorBasic() {
-		Heap<Integer> heap1 = new ArrayHeap<>(3, (a, b) -> -Integer.compare(a, b));
-		Heap<Integer> heap2 = new ArrayHeap<>(3, (a, b) -> Integer.compare(a, b));
-
-		IntStream.range(0, 100)
-				.forEach(heap1::offer);
-		IntStream.range(0, 100)
-				.forEach(heap2::offer);
-
-		for (int i = 0; i < 100; i++) {
-			assertEquals("Custom comparator is not working (reverse order)", 99 - i, (int) heap1.poll());
-			assertEquals("Custom comparator is not working (natural order)", i, (int) heap2.poll());
-		}
 	}
 }
